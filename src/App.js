7ee69964d22b91a602eb50cc653ed28d4bfd7747
Miguel Arcanjo <mig.ramos10@gmail.com>
2022-04-17 00:50:01 -0300

    Uso de children

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Hooks from './components/Hooks';
 import List from './components/List';
 import RenderCond from './components/RenderCond';
 import Fragment from './components/Fragment';
+import Container from './components/Container';
 
 
 function App() {
@@ -19,6 +20,9 @@ function App() {
       <List />
       <RenderCond x={8} y={10}/>
       <Fragment />
+      <Container>
+        <h1>Este é filho do Container</h1>
+      </Container>
     </div>
   );
 }
