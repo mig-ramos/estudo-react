78dfb7f9424a0c807dd97c05f2624eda1b918d9b
Miguel Arcanjo <mig.ramos10@gmail.com>
2022-04-16 23:06:13 -0300

    Componente Image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import FirstComponent from './components/FirstComponent';
 import AnotherComponent from './components/AnotherComponent';
+import Images from './components/Images';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <h2>Hello React</h2>
       <FirstComponent />
       <AnotherComponent />
+      <Images />
     </div>
   );
 }
---
+++ src/components/Images.js
@@ -0,0 +1,12 @@
+import mountain from '../assets/f5.jpg'
+
+const Images = () => {
+    return (
+        <div>
+            <img src="/f2.jpg" alt="Foto f2" />
+            <img src={mountain} alt='' />
+        </div>
+    )
+}
+
+export default Images;
